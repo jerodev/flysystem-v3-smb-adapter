8804b7bd257b6d88f6142e943cd11f5a9a994832
jerodev <jeroen@deviaene.eu>
2022-02-15 18:05:39 +0100

    Fix Laravel adapter

--- src/LaravelSmbAdapterProvider.php
+++ src/LaravelSmbAdapterProvider.php
@@ -4,6 +4,7 @@ namespace Jerodev\Flysystem\Smb;
 
 use Icewind\SMB\BasicAuth;
 use Icewind\SMB\ServerFactory;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
@@ -13,10 +14,18 @@ class LaravelSmbAdapterProvider extends ServiceProvider
     public function register()
     {
         Storage::extend('smb', static function ($app, $config) {
-            $server = (new ServerFactory())->createServer($config['host'], new BasicAuth($config['username'], $config['workgroup'], $config['password']));
+            $server = (new ServerFactory())->createServer(
+                $config['host'],
+                new BasicAuth($config['username'], $config['workgroup'] ?? 'WORKGROUP', $config['password'])
+            );
             $share = $server->getShare($config['path']);
+            $adapter = new SmbAdapter($share);
 
-            return new Filesystem(new SmbAdapter($share), $config);
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config,
+            );
         });
     }
 }
